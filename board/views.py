c9662157debbbef18a4de8fa5fa19467801e485b
Narsimha reddy <manjulareddy0907@gmail.com>
2025-07-04 23:47:44 +0530

    Add user authentication with signup, login, logout

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class MessageListView(ListView):
@@ -12,18 +13,18 @@ class MessageDetailView(DetailView):
     model = Message
     template_name = 'message_detail.html'
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
     model = Message
     template_name = 'message_form.html'
     fields = ['title', 'body']
     success_url = reverse_lazy('message_list')
 
-class MessageUpdateView(UpdateView):
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
     model = Message
     template_name = 'message_form.html'
     fields = ['title', 'body']
 
-class MessageDeleteView(DeleteView):
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
     model = Message
     template_name = 'message_confirm_delete.html'
     success_url = reverse_lazy('message_list')
